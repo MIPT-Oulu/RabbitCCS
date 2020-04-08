535ffa726bfdc2db0be89f68a5911b039d284398
sarytky <sarytky@student.oulu.fi>
2020-04-08 11:24:07 +0300

    2D evaluation update

--- scripts/evaluate_metrics.py
+++ scripts/evaluate_metrics.py
@@ -22,45 +22,38 @@ cv2.setNumThreads(0)
 
 if __name__ == "__main__":
     start = time()
-    pred_path = Path('/media/dios/dios2/RabbitSegmentation/µCT/Used in method manuscript for CC segmentation/')
     snapshot = Path('dios-erc-gpu_2019_11_19_15_45_01_hist_validation')
+    base_path = Path('../../../Data/')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mask_path', type=Path, default='../../../Data/masks')
-    parser.add_argument('--image_path', type=Path, default='../../../Data/images')
-    parser.add_argument('--prediction_path', type=Path, default='/media/dios/dios2/RabbitSegmentation/Histology/Rabbits/Predictions_CTRL_crop_outoffold/Largest')
-    parser.add_argument('--save_dir', type=Path, default='/media/dios/dios2/RabbitSegmentation/results/evaluation_outoffold')
-    parser.add_argument('--eval_name', type=str, default='histo_outoffold')
+    parser.add_argument('--mask_path', type=Path, default=base_path / 'masks')
+    parser.add_argument('--image_path', type=Path, default=base_path / 'images')
+    parser.add_argument('--prediction_path', type=Path, default=base_path / 'predictions')
+    parser.add_argument('--save_dir', type=Path, default=base_path / 'evaluation')
     parser.add_argument('--n_threads', type=int, default=16)
     parser.add_argument('--n_labels', type=int, default=2)
-    parser.add_argument('--experiment', default='./experiment_config.yml')
-    parser.add_argument('--snapshot', type=Path,
-                        default=Path('../../../workdir/snapshots/') / snapshot)
-    parser.add_argument('--dtype', type=str, choices=['.bmp', '.png', '.tif'], default='.bmp')
     args = parser.parse_args()
 
-    # Load snapshot configuration
-    with open(args.snapshot / 'config.yml', 'r') as f:
-        config = yaml.load(f, Loader=yaml.Loader)
+    # Snapshots to be evaluated
+    snaps = os.listdir(str(args.prediction_path))
+    snaps.sort()
+    snaps = [snaps[-1]]
 
-    with open(args.snapshot / 'args.dill', 'rb') as f:
-        args_experiment = dill.load(f)
-
-    with open(args.snapshot / 'split_config.dill', 'rb') as f:
-        split_config = dill.load(f)
-
-    # Initialize results
-    results = {'Sample': [], 'Dice': [], 'IoU': [], 'Similarity': []}
-
-    # Loop for samples
+    # Iterate through snapshots
     args.save_dir.mkdir(exist_ok=True)
-    #samples = [os.path.basename(x) for x in glob(str(args.mask_path / '*XZ'))]
-    samples_mask = os.listdir(str(args.mask_path))
-    samples_pred = os.listdir(str(args.prediction_path))
-    samples_mask.sort()
-    samples_pred.sort()
-    for idx, sample in enumerate(samples_mask):
-        try:
+    for snap in snaps:
+
+        # Initialize results
+        results = {'Sample': [], 'Dice': [], 'IoU': [], 'Similarity': []}
+
+        # Loop for samples
+        args.save_dir.mkdir(exist_ok=True)
+        #samples = [os.path.basename(x) for x in glob(str(args.mask_path / '*XZ'))]
+        samples_mask = os.listdir(str(args.mask_path))
+        samples_pred = os.listdir(str(args.prediction_path))
+        samples_mask.sort()
+        samples_pred.sort()
+        for idx, sample in enumerate(samples_mask):
             sleep(0.5); print(f'==> Processing sample {idx + 1} of {len(samples_mask)}: {sample}')
 
             # Load image stacks
@@ -87,20 +80,16 @@ if __name__ == "__main__":
             results['IoU'].append(iou)
             results['Similarity'].append(sim)
 
-        except Exception as e:
-            print(f'Sample {sample} failing due to error:\n\n{e}\n!')
-            continue
+        # Add average value to
+        results['Sample'].append('Average values')
+        results['Dice'].append(np.average(results['Dice']))
+        results['IoU'].append(np.average(results['IoU']))
+        results['Similarity'].append(np.average(results['Similarity']))
 
-    # Add average value to
-    results['Sample'].append('Average values')
-    results['Dice'].append(np.average(results['Dice']))
-    results['IoU'].append(np.average(results['IoU']))
-    results['Similarity'].append(np.average(results['Similarity']))
-
-    # Write to excel
-    writer = pd.ExcelWriter(str(args.save_dir / ('metrics_' + str(snapshot) + '_' + args.eval_name)) + '.xlsx')
-    df1 = pd.DataFrame(results)
-    df1.to_excel(writer, sheet_name='Metrics')
-    writer.save()
+        # Write to excel
+        writer = pd.ExcelWriter(str(args.save_dir / ('metrics_' + str(snap))) + '.xlsx')
+        df1 = pd.DataFrame(results)
+        df1.to_excel(writer, sheet_name='Metrics')
+        writer.save()
 
     print(f'Metrics evaluated in {(time() - start) // 60} minutes, {(time() - start) % 60} seconds.')
